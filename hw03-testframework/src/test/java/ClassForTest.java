0e393ab2221fcf379f006c7964ef006ea3e02e3d
Medvedev Alexandr <medvedev.alexandr88@yandex.ru>
2024-08-04 13:57:24 +0300

    fix code in third homework after review

--- hw03-testframework/src/test/java/ClassForTest.java
+++ hw03-testframework/src/test/java/ClassForTest.java
@@ -3,19 +3,6 @@ import annotation.*;
 import static org.assertj.core.api.Assertions.*;
 
 public class ClassForTest {
-
-    @BeforeAll
-    public static void initAll() {
-        System.out.println("Before all tests");
-        //throw new RuntimeException("Oooppss in initAll (@BeforeAll)");
-    }
-
-    @AfterAll
-    public static void tearDownAll() {
-        System.out.println("After all tests");
-
-    }
-
     @Before
     public void init() {
         System.out.println("Before each test");
@@ -37,6 +24,7 @@ public class ClassForTest {
     @Test
     public void secondTest() {
         System.out.println("Running test 2");
+        //throw new RuntimeException("Oooppss in firstTest");
         assertThat(1 + 1).isEqualTo(2);
     }
 
