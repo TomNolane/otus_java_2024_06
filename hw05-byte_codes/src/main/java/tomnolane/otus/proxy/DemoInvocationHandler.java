15a60e0b3adb19bb75693dba00ae8a1382ee0245
Medvedev Alexandr <medvedev.alexandr88@yandex.ru>
2024-08-17 19:18:05 +0300

    fix homework after review

--- hw05-byte_codes/src/main/java/tomnolane/otus/proxy/DemoInvocationHandler.java
+++ hw05-byte_codes/src/main/java/tomnolane/otus/proxy/DemoInvocationHandler.java
@@ -11,35 +11,31 @@ import java.util.*;
 public class DemoInvocationHandler implements InvocationHandler {
     private static final Logger logger = LoggerFactory.getLogger(DemoInvocationHandler.class);
     private final Object myClass;
-    private final ArrayList<Method> annotatedMethods = new ArrayList<>();
+    private final Set<String> annotatedMethods = new HashSet<>();
 
     DemoInvocationHandler(Object myClass) {
         this.myClass = myClass;
 
         for (Method method : myClass.getClass().getDeclaredMethods()) {
             if (method.isAnnotationPresent(Log.class)) {
-                annotatedMethods.add(method);
+                annotatedMethods.add(method.getName() + Arrays.toString(method.getParameterTypes()));
             }
         }
     }
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        for (Method annotatedMethod : annotatedMethods) {
-            if (annotatedMethod.getName().equals(method.getName()) &&
-                    annotatedMethod.getReturnType() == method.getReturnType() &&
-                    Arrays.equals(annotatedMethod.getParameterTypes(), method.getParameterTypes())
-            ) {
-                StringBuilder stringArgs = new StringBuilder();
-                for (int i = 0; i < args.length; i++) {
-                    stringArgs.append(args[i]);
-                    if (i < args.length - 1) {
-                        stringArgs.append(", ");
-                    }
+        final String key = method.getName() + Arrays.toString(method.getParameterTypes());
+        final StringBuilder stringArgs = new StringBuilder();
+        if(annotatedMethods.contains(key)) {
+            for (int i = 0; i < args.length; i++) {
+                stringArgs.append(args[i]);
+                if (i < args.length - 1) {
+                    stringArgs.append(", ");
                 }
-                logger.info("executed method: {}, param: {}", method.getName(), stringArgs);
             }
         }
+        logger.info("executed method: {}, param: {}", method.getName(), stringArgs);
 
         return method.invoke(myClass, args);
     }
