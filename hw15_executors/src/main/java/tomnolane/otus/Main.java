bc2e69ddcfcf011392ecc2e12473cb89e9a346b0
Medvedev Alexandr <medvedev.alexandr88@yandex.ru>
2024-12-15 17:54:50 +0300

    fix code after code review

--- hw15_executors/src/main/java/tomnolane/otus/Main.java
+++ hw15_executors/src/main/java/tomnolane/otus/Main.java
@@ -3,16 +3,11 @@ package tomnolane.otus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 public class Main {
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
-    private static List<Integer> array = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
     private boolean isNeedToReverse = true;
-    private static int lastFirst = -1;
-    private static int lastSecond = -1;
+    private final static int lastFirst = 0;
+    private final static int lastSecond = 0;
     private String currentTurn = "Thread 1";
 
     public static void main(String[] args) {
@@ -22,35 +17,31 @@ public class Main {
         new Thread(() -> pingPong.action(lastSecond), "Thread 2").start();
     }
 
-    private synchronized void action(Integer threadIndex) {
+    private synchronized void action(int threadIndex) {
         while (!Thread.currentThread().isInterrupted()) {
             try {
-
                 while (!Thread.currentThread().getName().equals(currentTurn)) {
                     this.wait();
                 }
 
-                if (threadIndex == -1) { //for init counting
-                    threadIndex = 0;
-                } else if (threadIndex == array.size()) {
-                    threadIndex = 1;
-
-                    if(isNeedToReverse) {
-                        Collections.reverse(array);
-                        isNeedToReverse = false;
-                    } else {
-                        isNeedToReverse = true;
-                    }
+                if (threadIndex == 0) {
+                    isNeedToReverse = false;
+                } else if (threadIndex == 10) {
+                    isNeedToReverse = true;
                 }
 
-                logger.info("{} - {}", Thread.currentThread().getName(), array.get(threadIndex));
+                if(isNeedToReverse) {
+                    threadIndex -= 1;
+                } else {
+                    threadIndex += 1;
+                }
 
-                threadIndex += 1;
+                logger.info("{} - {}", Thread.currentThread().getName(), threadIndex);
 
                 currentTurn = Thread.currentThread().getName().equals("Thread 1") ? "Thread 2" : "Thread 1";
 
                 this.notifyAll();
-                Thread.sleep(1_000);
+                Thread.sleep(500);
 
             } catch (InterruptedException ex) {
                 Thread.currentThread().interrupt();
